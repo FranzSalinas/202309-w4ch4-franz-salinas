edc1bda1df33241e2060e071b78ad34bdf3df5c1
Franz <fernandofranquiz@gmail.com>
2023-11-02 22:07:43 +0100

    beggining of context

--- src/components/key/key.tsx
+++ src/components/key/key.tsx
@@ -2,34 +2,54 @@ export function Key() {
   return (
     <>
       <li>
-        <button className="key">1</button>
+        <button className="key" onClick={() => handleClick()}>
+          1
+        </button>
       </li>
       <li>
-        <button className="key">2</button>
+        <button className="key" onClick={() => handleClick()}>
+          2
+        </button>
       </li>
       <li>
-        <button className="key">3</button>
+        <button className="key" onClick={() => handleClick()}>
+          3
+        </button>
       </li>
       <li>
-        <button className="key">4</button>
+        <button className="key" onClick={() => handleClick()}>
+          4
+        </button>
       </li>
       <li>
-        <button className="key">5</button>
+        <button className="key" onClick={() => handleClick()}>
+          5
+        </button>
       </li>
       <li>
-        <button className="key">6</button>
+        <button className="key" onClick={() => handleClick()}>
+          6
+        </button>
       </li>
       <li>
-        <button className="key">7</button>
+        <button className="key" onClick={() => handleClick()}>
+          7
+        </button>
       </li>
       <li>
-        <button className="key">8</button>
+        <button className="key" onClick={() => handleClick()}>
+          8
+        </button>
       </li>
       <li>
-        <button className="key">9</button>
+        <button className="key" onClick={() => handleClick()}>
+          9
+        </button>
       </li>
       <li>
-        <button className="key">0</button>
+        <button className="key" onClick={() => handleClick()}>
+          0
+        </button>
       </li>
       <li>
         <button className="key big">delete</button>
