af8cbcef7ed9130b28bfa0495436f580fc513cd0
Franz <fernandofranquiz@gmail.com>
2023-11-02 19:27:05 +0100

    Add display of all the components

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './src/app/app';
+import './src/styles/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,16 @@
+import { Actions } from '../components/actions/actions';
+import { Info } from '../components/info/info';
+import { Keyboard } from '../components/keyboard/keyboard';
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <Info></Info>
+      <main className="phone">
+        <Keyboard></Keyboard>
+        <Actions></Actions>
+      </main>
+      ;
+    </>
+  );
 }
