6b97c005687b972ae1805a764512c0596683f122
Franz <fernandofranquiz@gmail.com>
2023-11-03 09:56:41 +0100

    Fixing issue with vercel

--- src/components/context/phone-context.tsx
+++ src/components/context/phone-context.tsx
@@ -1,5 +1,10 @@
 import { createContext } from 'react';
 
-export type phoneContext = {
+export type PhoneContext = {
   phoneNumber: string;
+  setPhoneNumber: (phoneNumber: string) => void;
 };
+
+const initialContext = {} as PhoneContext;
+
+export const ThePhoneContext = createContext<PhoneContext>(initialContext);
